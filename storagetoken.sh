36c6a661673f8d2adbf50d782dc36c9e8e0ef281
bartuer zhou <bazhou@microsoft.com>
2016-10-05 05:18:29 +0800

    token utils fix

--- storagetoken.sh
+++ storagetoken.sh
@@ -5,5 +5,10 @@ azure vm list >/dev/null 2>&1 || {
     exit 1;
 }
 
+if [ -z "$1" ]; then
+    echo "Specify machine name: ./storagetoken.sh the_machine_name"
+    exit 2;
+fi
+
 node get_azure_token.js | ssh $1 'cat - > /etc/default/azurefile-dockervolumedriver/azurefile-dockervolumedriver.default'
 curl -o- https://raw.githubusercontent.com/bartuer/CAAPI_provision/master/createdockervolume.sh | ssh $1 'bash -s'
