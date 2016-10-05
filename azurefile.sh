d645a5c46e22189f3162acdb981b635828449a24
bartuer zhou <bazhou@microsoft.com>
2016-10-05 14:42:42 +0800

    fix source

--- azurefile.sh
+++ azurefile.sh
@@ -8,7 +8,7 @@ sudo mkdir -p /etc/default/azurefile-dockervolumedriver
 sudo curl https://raw.githubusercontent.com/Azure/azurefile-dockervolumedriver/master/contrib/init/systemd/azurefile-dockervolumedriver.default -o /etc/default/azurefile-dockervolumedriver/azurefile-dockervolumedriver.default
 sudo chmod g+w /etc/default/azurefile-dockervolumedriver/azurefile-dockervolumedriver.default
 sudo curl https://raw.githubusercontent.com/bartuer/CAAPI_provision/master/dockervolumedriver.service -o /usr/lib/systemd/system/azurefile-dockervolumedriver.service
-sudo ln -s /usr/lib/systemd/system/azurefile-dockervolumedriver.service /etc/systemd/system/multi-user.target.wants/azurefile-dockervolumedriver.service
+sudo ln -s /etc/systemd/system/multi-user.target.wants/azurefile-dockervolumedriver.service /usr/lib/systemd/system/azurefile-dockervolumedriver.service 
 sudo systemctl daemon-reload
 sudo systemctl enable azurefile-dockervolumedriver
 sudo systemctl start azurefile-dockervolumedriver
