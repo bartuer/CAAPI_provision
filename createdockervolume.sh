90213d91c8c629c4a0c54a6b9334a2004fdbcba2
bartuer zhou <bazhou@microsoft.com>
2016-10-09 11:24:21 +0800

    ensure AFS started

--- createdockervolume.sh
+++ createdockervolume.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+sudo systemctl daemon-reload
+sudo systemctl enable azurefile-dockervolumedriver
+sudo systemctl start azurefile-dockervolumedriver
 docker volume create -d  azurefile -o share=code --name=codevol
 docker volume create -d  azurefile -o share=data --name=datavol
