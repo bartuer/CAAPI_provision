b46a2de42217a6ef5f2aa64e5e4ca9f918eea21e
bartuer zhou <bazhou@microsoft.com>
2016-10-14 09:15:54 +0800

    machine_ip -> machine_name-ip

--- ssh_config.js
+++ ssh_config.js
@@ -2,9 +2,11 @@ var fs = require('fs');
 var sep = require('path').sep;
 var exec = require('child_process').execSync;
 var machine_parameter = JSON.parse(fs.readFileSync([process.cwd(), 'parameters.json'].join(sep)));
+
 var user_name = machine_parameter.parameters.virtualMachines_nixml_adminUsername.value;
 var machine_name = machine_parameter.parameters.virtualMachines_nixml_name.value;
-var address = JSON.parse(exec(`azure network public-ip show CAAPI ${machine_name}-ip --json`).toString());
+var machine_ip = machine_parameter.parameters.publicIPAddresses_nixml_ip_name;
+var address = JSON.parse(exec(`azure network public-ip show CAAPI ${machine_ip} --json`).toString());
 var ip = address.ipAddress;
 var dns_name = address.dnsSettings.fqdn;
 console.log('');
